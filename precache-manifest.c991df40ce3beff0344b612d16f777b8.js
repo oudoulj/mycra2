c4ce953c54162f8f90955ba56e2ab840622e341c
Jérôme OUDOUL <jerome@oudoul.com>
2018-10-11 00:41:29 +0200

    Initial Commit

---
+++ precache-manifest.c991df40ce3beff0344b612d16f777b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mycra2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "37b5a2ff932de3e768b0",
+    "url": "/mycra2/static/js/runtime~main.37b5a2ff.js"
+  },
+  {
+    "revision": "67bae552246991bd2b7a",
+    "url": "/mycra2/static/js/main.67bae552.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/mycra2/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "67bae552246991bd2b7a",
+    "url": "/mycra2/static/css/main.f60932db.chunk.css"
+  },
+  {
+    "revision": "addbdb5eab60c663ff92bcea36259c5f",
+    "url": "/mycra2/index.html"
+  }
+];
